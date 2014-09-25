318415da729e01a61d0b9c65a33390b9317a9ce9
Kyle Boulay <kyleboulay@gmail.com>
2014-09-25 01:49:18 -0400

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module R2SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # Added by KYLE
+    # Add a line for asset pipeline compatiblity
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
